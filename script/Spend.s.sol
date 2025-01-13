baf0c8a1b087fce320665e523b9f10a2c59ca314
mosnyik <mosnyik@gmail.com>
2025-01-13 13:49:55 +0100

    for pipeline access

--- script/Spend.s.sol
+++ script/Spend.s.sol
@@ -1,25 +1,25 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.25;
 
-import {Script, console} from "forge-std/Script.sol";
-import {SpendCoin} from "../src/SpendCoin.sol";
+// import {Script, console} from "forge-std/Script.sol";
+// import {SpendCoin} from "../src/SpendCoin.sol";
 
-contract Spend is Script {
-    function run() external {
-        // deployment private key from env
-        uint256 deploymentPrivateKey = vm.envUint("LOCAL_TEST_NET_PRIVATE_KEY");
-        address recipient = vm.parseAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+// contract Spend is Script {
+//     function run() external {
+//         // deployment private key from env
+//         uint256 deploymentPrivateKey = vm.envUint("LOCAL_TEST_NET_PRIVATE_KEY");
+//         address recipient = vm.parseAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
 
-        // broadcast the transaction using the deployerPrivateKey
-        vm.startBroadcast(deploymentPrivateKey);
+//         // broadcast the transaction using the deployerPrivateKey
+//         vm.startBroadcast(deploymentPrivateKey);
 
-        // Deploy the contract
-        SpendCoin spendCoin = new SpendCoin(payable(recipient));
+//         // Deploy the contract
+//         SpendCoin spendCoin = new SpendCoin(payable(recipient));
 
-        // log the deployed contract address in the console
-        console.log("Deployed contract address:", address(spendCoin));
+//         // log the deployed contract address in the console
+//         console.log("Deployed contract address:", address(spendCoin));
 
-        // stop braodcasting the transaction
-        vm.stopBroadcast();
-    }
-}
+//         // stop braodcasting the transaction
+//         vm.stopBroadcast();
+//     }
+// }
